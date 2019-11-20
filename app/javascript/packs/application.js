83c20ba20fca41b98a3d74a2dee576c5e8c668f7
Claudine <clo.pradal@gmail.com>
2019-11-20 10:13:18 +0100

    create app

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,9 @@
+import "bootstrap";
+
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+import { initMapbox } from '../plugins/init_mapbox';
+import { initAutocomplete } from '../plugins/init_autocomplete';
+
+initMapbox();
+initAutocomplete();
